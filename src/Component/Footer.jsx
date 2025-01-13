8b0c519c9b7c50788fbd1bca465c887b4863fc00
Preeti <p208016@gmail.com>
2025-01-13 15:00:19 +0530

    code

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-function Footer(){
-    return(
-        <>
-        <div>
-            <div className="footer">
-                <ul>
-                    <li><Link to = "/">Home</Link></li>
-                    <li><Link to = "/Winter">Winter</Link></li>
-                    <li><Link to = "/Summer">Summer</Link></li>
-                    <li><Link to = "/Spring">Spring</Link></li>
-                    <li><Link to = "/Autumn">Autumn</Link></li>
-                </ul>
-            </div>
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+
+function Footer() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <div className="footer">
+        <div className="hamburger" onClick={toggleMenu}>
+          ☰
         </div>
-        </>
-    );
+        <ul className={menuOpen ? "menu active" : "menu"}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/Winter">Winter</Link></li>
+          <li><Link to="/Summer">Summer</Link></li>
+          <li><Link to="/Spring">Spring</Link></li>
+          <li><Link to="/Autumn">Autumn</Link></li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
